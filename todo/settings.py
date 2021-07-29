116714f8b0694fbb8aab6196753ceda9d667e0bf
charles <charlesmjmtm@gmail.com>
2021-07-29 12:48:41 +0530

    allowed hosts

--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--xl#y!*6)uhbkujod4d@vhcu@2ey#m9=!vifijcq)d9(f!!^q_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['*'] 
 
 
 # Application definition
